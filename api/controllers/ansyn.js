ebfd63a69d49a6cc5728d076f0601df2563e3349
YairTawil <yairtawil@gmail.com>
2018-11-07 11:11:35 +0200

    feat: ansyn api yamls (#6)
    * feat: ansyn api yamls
    
    * finish configure yamls
    
    * fix: CR request changes
    
    * fix: remove using http

---
+++ api/controllers/ansyn.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+	uploadImage
+};
+
+function uploadImage(req, res) {
+	res.json({ message: 'great!' });
+}
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const SwaggerExpress = require('swagger-express-mw');
+const SwaggerUi = require('swagger-tools/middleware/swagger-ui');
+const express = require('express');
+const cors = require('cors');
+const { port, yamlsPort, yamlsPath } = require('./config/config');
+
+const app = express();
+const yamls = express();
+
+const config = {
+	appRoot: __dirname
+};
+
+/* yamls */
+yamls.use(cors());
+yamls.use(yamlsPath, express.static(__dirname + '/api/yamls'));
+yamls.listen(yamlsPort, () => {
+	console.log(`Yamls listen on port: ${yamlsPort}`);
+});
+
+SwaggerExpress.create(config, function (err, swaggerExpress) {
+	if (err) {
+		throw err;
+	}
+
+	/* app */
+	app.use(SwaggerUi(swaggerExpress.runner.swagger));
+	swaggerExpress.register(app);
+	app.use(cors());
+	app.listen(port, () => {
+		console.log(`Swagger-ui available on ${port}, on: http://localhost:${port}/docs`);
+	});
+
+
+});
