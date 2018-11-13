ac76c669bac33a0bb2bdcb3dfcebe0566a7b2cca
YairTawil <yairtawil@gmail.com>
2018-11-13 17:03:26 +0200

    Relative paths (#14)
    * upload files
    
    * remove comment server (swagger yaml)
    
    * feat: relative path

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const SwaggerExpress = require('swagger-express-mw');
 const SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 const express = require('express');
@@ -10,58 +9,61 @@ const { mongodb, swagger, remote } = require('./config/config');
 const api = require('./src/api/index');
 const login = require('./src/login/index');
 const DBManager = require('./src/database/DBManager');
+const JsonRefs = require('json-refs');
+const YAML = require('js-yaml');
 
-
-const yamls = express();
-const app = express();
-
-const config = {
-	appRoot: __dirname
-};
-
-/* yamls */
-yamls.use(cors());
-yamls.use(swagger.yamls.path, express.static(__dirname + swagger.yamls.staticUrl));
-yamls.listen(swagger.yamls.port, () => {
-	console.log(`Yamls listen on port: ${swagger.yamls.port}`);
-});
-
-SwaggerExpress.create(config, function (err, swaggerExpress) {
-	if (err) {
-		throw err;
+JsonRefs.resolveRefsAt('./api/swagger/swagger.yaml', {
+	filter: ['relative', 'remote'],
+	loaderOptions: {
+		processContent: (res, cb) => cb(undefined, YAML.safeLoad(res.text))
 	}
+}).then((results) => {
+	const app = express();
 
-	/* swaggerUi */
-	app.use(SwaggerUi(swaggerExpress.runner.swagger));
-	swaggerExpress.register(app);
-	app.use(cors());
+	const config = {
+		appRoot: __dirname,
+		swagger: results.resolved
+	};
 
-	/* v1 api - no swagger */
 
-	// DB Connection URL
-	const url = `${mongodb.url}/${mongodb.name}`;
-
-	// start the connection to the mongo Database
-	DBManager.connect(url).catch(() => {
-		console.log("No connection for mongo!")
-	});
-	app.use(cors({ credentials: true, origin: true }));
-	app.use(bodyParser.json());
-
-	app.use(session({
-		secret: 'keyboard cat',
-		resave: false,
-		saveUninitialized: true,
-		cookie: {
-			secure: false
+	SwaggerExpress.create(config, function (err, swaggerExpress) {
+		if (err) {
+			throw err;
 		}
-	}));
 
-	app.use('/login', login);
-	app.use('/v1/api', api);
+		/* swaggerUi */
+		app.use(SwaggerUi(swaggerExpress.runner.swagger));
+		swaggerExpress.register(app);
+		app.use(cors());
 
-	app.listen(swagger.port, () => {
-		console.log(`Swagger-ui available on ${swagger.port}, on: ${remote.domain}:${swagger.port}/docs`);
+		/* v1 api - no swagger */
+
+		// DB Connection URL
+		const url = `${mongodb.url}/${mongodb.name}`;
+
+		// start the connection to the mongo Database
+		DBManager.connect(url).catch(() => {
+			console.log('No connection for mongo!');
+		});
+		app.use(cors({ credentials: true, origin: true }));
+		app.use(bodyParser.json());
+
+		app.use(session({
+			secret: 'keyboard cat',
+			resave: false,
+			saveUninitialized: true,
+			cookie: {
+				secure: false
+			}
+		}));
+
+		app.use('/login', login);
+		app.use('/v1/api', api);
+
+		app.listen(swagger.port, () => {
+			console.log(`Swagger-ui available on ${swagger.port}, on: ${remote.domain}:${swagger.port}/docs`);
+		});
+
 	});
-
 });
+
