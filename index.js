ad306f2d2de509a874456e0e8997229efef6d6ad
dafnaweiss <dafna@webiks.com>
2018-11-15 13:33:15 +0200

    change all the yaml files to JSON files

--- index.js
+++ index.js
@@ -6,38 +6,20 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const $RefParser = require('json-schema-ref-parser');
-const JsonRefs = require('json-refs');
-const YAML = require('js-yaml');
 const { mongodb, appPort, remote } = require('./config/config');
 const api = require('./src/api/index');
 const login = require('./src/login/index');
 const DBManager = require('./src/database/DBManager');
 
-// JsonRefs.resolveRefsAt('./api/swagger/swagger.yaml', {
-// 	filter: ['relative', 'remote'],
-// 	loaderOptions: {
-// 		processContent: (res, cb) => cb(undefined, YAML.safeLoad(res.text))
-// 	}
-// })
-// JsonRefs.resolveRefsAt('./api/swagger/swagger.json', {
-// 	filter: ['relative', 'remote'],
-// 	loaderOptions: {
-// 		processContent: (res, cb) => cb(undefined, YAML.safeLoad(res, { schema: 'JSON_SCHEMA'}))
-// 	}
-// })
-JsonRefs.resolveRefsAt('./api/swagger/swagger.json', {filter: ['relative', 'remote']})
-//$RefParser.dereference('./api/swagger/swagger.json')
+$RefParser.dereference('./api/swagger/swagger.json')
 	.then(results => {
 		const app = express();
 		const config = {
 			appRoot: __dirname,
-			swagger: results.resolved
+			swagger: results
 		};
-		console.log(`config appRoot: ${config.appRoot}`);
-		console.log(`config swagger: ${JSON.stringify(config.swagger)}`);
 
 		SwaggerExpress.create(config, function (err, swaggerExpress) {
-			console.log(`start SwaggerExpress create...`);
 			if (err) {
 				throw err;
 			}
