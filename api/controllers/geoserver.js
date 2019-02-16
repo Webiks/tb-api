4c85b16ab79701a4118c7ceb5f4889e81bac873d
YairTawil <yairtawil@gmail.com>
2019-02-16 19:36:19 +0200

    fix: lint

--- api/controllers/geoserver.js
+++ api/controllers/geoserver.js
@@ -3,19 +3,10 @@ const { geoserver } = require('../../config/config');
 
 const getCapabilities = (req, res) => {
 	const { workspace: { value: workspace }, layer: { value: layer }} = req.swagger.params;
-
-	axios.get(`${geoserver.url}/${workspace}/${layer}/gwc/service/wmts`, {
-		params: {
-			SERVICE: 'WMTS',
-			REQUEST: 'getCapabilities',
-			VERSION: '1.1.1'
-		}
-	}).then((capabilities) => {
-		res.send(capabilities.data)
-	}).catch((err) => {
-		console.log(err.message);
-		res.status(500).send({ error: err.message });
-	});
+	const params = { SERVICE: 'WMTS', REQUEST: 'getCapabilities', VERSION: '1.1.1' };
+	axios.get(`${geoserver.url}/${workspace}/${layer}/gwc/service/wmts`, { params })
+		.then((capabilities) => res.send(capabilities.data))
+		.catch((err) => res.status(500).send({ error: err.message }));
 };
 
 module.exports = { getCapabilities };
