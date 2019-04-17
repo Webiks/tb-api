8398c82e71acb78d4e44b364898fddb2ed4e29f4
Tzahi Levi <thetzahilevi@gmail.com>
2019-04-17 11:25:33 +0300

    get credentials from ecs

--- src/controllers/utils/s3/getS3Object.js
+++ src/controllers/utils/s3/getS3Object.js
@@ -6,19 +6,19 @@ aws.config.logger = console;
 
 let myCredentials;
 // get the credentials
-if (process.env.NODE_ENV === 'production') {
-	// remote - get temporary credentials from the ec2
-	myCredentials = new aws.EC2MetadataCredentials({
-		httpOptions: { timeout: 5000 }, // 5 second timeout
-		maxRetries: 10, // retry 10 times
-		retryDelayOptions: { base: 200 }
-	});
-} else {
+if (process.env[s3config.accessKeyId] && process.env[s3config.secretAccessKey]) {
 	// local - get credentials from the Environment Variables
 	myCredentials = {
 		accessKeyId: process.env[s3config.accessKeyId],
 		secretAccessKey: process.env[s3config.secretAccessKey]
 	};
+} else {
+	// remote - get temporary credentials from the ec2
+	myCredentials = new aws.ECSCredentials({
+		httpOptions: { timeout: 5000 }, // 5 second timeout
+		maxRetries: 10, // retry 10 times
+		retryDelayOptions: { base: 200 }
+	});
 }
 
 // configure the s3 service object
